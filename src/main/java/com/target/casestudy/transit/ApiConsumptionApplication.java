9053aee3d0c8d00ef88c7311e0f0fd881b813eb7
Srinivas114 <srinivas.varee@gmail.com>
2019-06-29 12:42:04 -0500

    Initial commit

---
+++ src/main/java/com/target/casestudy/transit/ApiConsumptionApplication.java
@@ -0,0 +1,13 @@
+package com.target.casestudy.transit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConsumptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConsumptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/target/casestudy/transit/ApiConsumptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.target.casestudy.transit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiConsumptionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
